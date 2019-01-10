4720048d215cd3560ba761f68cd39de3595d272d
Eric Winkle <elkniwcire@gmail.com>
2019-01-10 09:47:57 -0500

    - speed works - dots disappear on click - need to fix: game moves on speed change when paused - need to fix: buttons are clickable when paused

--- index.js
+++ index.js
@@ -1,112 +1,113 @@
 
 var createDotInterval;
 var moveDotInterval;
+var moveDotSpeed = 50;
+var score = 0;
+
+var button = document.querySelector("#play-pause-button");
+
+updateScore(score); //sets inital score value
+
+function updateScore(value){
+    var scoreText = document.querySelector("#score");
+    scoreText.innerHTML = value; // sets score text to value
+}
+
+var slider = document.querySelector("#slider"); //getting slider
+updateSpeedText(slider.value); //passing data update display text
+
+function updateSpeedText(value) {
+    var speedText = document.querySelector("#speed"); //selecting speed display text
+    speedText.innerHTML = value; //setting display text to match value of slider at startup
+}
+
+slider.oninput = function() {
+    value = this.value;   //getting value of slider on change
+    updateSpeedText(value);
+    var speed = 101 - value;
+    UpdateSpeed(speed);
+}
 
 function createDots() {
     createDotInterval = setInterval(createDot, 1000);
+
+    function createDot() {
+        var dot = document.createElement("a"); //create element
+        dot.className = "dot"; //giving it class dot
+        var randomSize = Math.floor((Math.random() * 100) + 10);
+        dot.style.height = randomSize + "px";
+        dot.style.width = randomSize + "px";
+        dot.onclick = function(){dotPressed(101-randomSize, this);};
+        var ballContainer = document.querySelector("#ball-container");
+        var min = ballContainer.offsetLeft;
+        var max = ballContainer.clientWidth - randomSize;
+        var randomLeft = Math.floor((Math.random() * max) + min);
+        var newPositionLeft = dot.offsetLeft + randomLeft;
+        dot.style.left = newPositionLeft + "px";
+        ballContainer.appendChild(dot); //adding dot
+    }
 }
 
-function moveDots(speed){
+function moveDots(){
 
-    moveDotInterval = setInterval(moveDot, speed);
+    moveDotInterval = setInterval(moveDot, moveDotSpeed);
 
     function moveDot () {
 
-        var dots = document.getElementsByClassName("dot");
+        var dots = document.querySelectorAll(".dot");
         var i;
         for (i=0; i < dots.length; i++){
             var position = dots[i].offsetTop;
-            position++;
-            dots[i].style.top = position + 'px';
+            if (position > window.innerHeight){
+                dots[i].parentElement.removeChild(dots[i]);
+                console.log("gone");
+                /*Removing dot if it goes off screen to save memory and performance*/
+            } else {
+                position++;
+                dots[i].style.top = position + "px";
+            }
+            
         }
     }
 }
 
-function createDot() {
-    var dot = document.createElement("a"); //create element
-    dot.className = "dot"; //giving it class dot
-    var randomSize = Math.floor((Math.random() * 100) + 10);
-    dot.style.height = randomSize + 'px';
-    dot.style.width = randomSize + 'px';
-    var ballContainer = document.getElementById("ball-container");
-    var min = ballContainer.offsetLeft;
-    var max = ballContainer.clientWidth - randomSize;
-    var randomLeft = Math.floor((Math.random() * max) + min);
-    var newPositionLeft = dot.offsetLeft + randomLeft;
-    dot.style.left = newPositionLeft + 'px';
-    var element = document.getElementById("ball-container"); // finding ball container
-    element.appendChild(dot); //adding dot
-/*     var dots = document.getElementsByClassName("dot");
-    var thisDot = dots[dots.length - 1];
-    moveDot(thisDot); */
-}
 
-/* function moveDot(dot) {
-    var position = dot.offsetTop;
 
-        var id = setInterval(frame, 10);
-        function frame() {
-            if (position == 1000) {
-              clearInterval(id);
-            } else {
-              position++; 
-              dot.style.top = position + 'px';
-            }
-          }
-} */
+function buttonPressed() {
 
-/* function moveDots() {
-    var dots = document.getElementsByClassName("dot");
-    var i;
-    for (i = 0; i < dots.length; i++) {
-        var dot = dots[i];
-        var position = dot.offsetTop;
-
-        var id = setInterval(frame, 10);
-        function frame() {
-            if (position == 350) {
-                clearInterval(id);
-            } else {
-              position++; 
-              dot.style.top = position + 'px';
-            }
-          }
-
+    if (button.value == "play") {
+        play();
+    } else if (button.value == "pause"){
+        pause();
     }
-} */
+
+}
 
 function play() {
     createDots();
-    moveDots(10);
+    moveDots();
+    button.value = "pause";
 }
 
 function pause() {
     clearInterval(createDotInterval);
     clearInterval(moveDotInterval);
+    button.value = "play";
+    var dots = document.querySelectorAll(".dot");
+    var i;
+    for (i = 0; i < dots.length; i++){
+        dots[i].disabled = true;
+    }
 }
 
-/* function moveDot(dot) {
-    
-    var position = dot.offsetTop;
-    
-    if (position == 500) {
-      clearInterval(id);
-    } else {
-      position = position + 1;
-      console.log(dot.offsetTop);
-      dot.style.top = position + 'px';
-    }
-  } 
+function dotPressed(value, thisDot){
+    score = score + value;
+    updateScore(score);
+    thisDot.parentElement.removeChild(thisDot);
+}
 
-function dotDrop() {
-    createDot(); //create dot
-
-    var dots = document.getElementsByClassName("dot"); //putting all dots into array
-    var i; // counter
-    for (i = 0; i < dots.length; i++) {
-
-        var dot = dots[i];
-
-        var id = setInterval(moveDot.bind(null,dot),10);
-    }
-  } */
+function UpdateSpeed(speed){
+    clearInterval(moveDotInterval);
+    moveDotSpeed = speed;
+    moveDots(moveDotSpeed);
+}
